4189eabc937a9c2390fcf226da9db2d85b8ba24c
Agents Assistant <steve.journey87@gmail.com>
2026-07-21 12:01:41 +0930

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.go
@@ -0,0 +1,28 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	var sb strings.Builder
+	for _, s := range strs {
+		sb.WriteString(strconv.Itoa(len(s)))
+		sb.WriteString("#")
+		sb.WriteString(s)
+	}
+	// 0#1#a5#hello
+	return sb.String()
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	k := len(encoded)
+	var i, j int
+	res := []string{}
+	for j < k {
+		if encoded[j] == '#' {
+			l, _ := strconv.Atoi(encoded[i:j])
+			res = append(res, encoded[j+1:j+1+l])
+			i = j+1+l
+			j = i
+		}
+		j++
+	}
+	return res
+}
