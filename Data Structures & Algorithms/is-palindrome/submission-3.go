910ccb027f8c6864491cd9ba5d04c40d6553be94
Agents Assistant <steve.journey87@gmail.com>
2026-07-27 11:57:57 +0930

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.go
@@ -0,0 +1,24 @@
+func isPalindrome(s string) bool {
+	var alphaNum [128]bool
+	for c := '0'; c <= '9'; c++ { alphaNum[c] = true }
+	for c := 'A'; c <= 'Z'; c++ { alphaNum[c] = true }
+	for c := 'a'; c <= 'z'; c++ { alphaNum[c] = true }
+	s = strings.ToLower(s)
+	i, j := 0, len(s)-1
+	for i < j {
+		if !alphaNum[s[i]] {
+			i++
+			continue
+		}
+		if !alphaNum[s[j]] {
+			j--
+			continue
+		}
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
