0f641a74d9998e5374815ef9bfaeb2cba32470f5
Agents Assistant <steve.journey87@gmail.com>
2026-08-04 19:49:30 +0930

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.go
@@ -0,0 +1,13 @@
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+	res[0] = 1
+	for i := 0; i < len(nums)-1; i++ {
+		res[i+1] = res[i]*nums[i]
+	}
+	postProd := 1
+	for j := len(nums)-1; j >= 0; j-- {
+		res[j] *= postProd
+		postProd *= nums[j]
+	}
+	return res
+}
